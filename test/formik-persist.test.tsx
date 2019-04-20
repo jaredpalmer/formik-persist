fb34a9c67a871888a0a88e48114a7cb21aa5031f
ANUJ SACHAN <anuj.sachan@akqa.com>
2019-04-20 19:47:31 +1000

    added session storage option, if user wants to clear the form if browser closes.

--- test/formik-persist.test.tsx
+++ test/formik-persist.test.tsx
@@ -7,8 +7,8 @@ import { Formik, FormikProps, Form } from 'formik';
 const noop = () => {};
 
 describe('Formik Persist', () => {
-  let node = document.createElement('div');
   it('attempts to rehydrate on mount', () => {
+    let node = document.createElement('div');
     (window as any).localStorage = {
       getItem: jest.fn(),
       setItem: jest.fn(),
@@ -34,5 +34,34 @@ describe('Formik Persist', () => {
     expect(window.localStorage.getItem).toHaveBeenCalled();
     injected.setValues({ name: 'ian' });
     expect(injected.values.name).toEqual('ian');
+  });
+
+  it('attempts to rehydrate on mount if session storage is true on props', () => {
+    let node = document.createElement('div');
+    (window as any).sessionStorage = {
+      getItem: jest.fn(),
+      setItem: jest.fn(),
+      removeItem: jest.fn(),
+    };
+    let injected: any;
+
+    ReactDOM.render(
+      <Formik
+        initialValues={{ name: 'Anuj Sachan' }}
+        onSubmit={noop}
+        render={(props: FormikProps<{ name: string }>) => {
+          injected = props;
+          return (
+            <div>
+              <Persist name="signup" debounce={0} isSessionStorage />
+            </div>
+          );
+        }}
+      />,
+      node
+    );
+    expect(window.sessionStorage.getItem).toHaveBeenCalled();
+    injected.setValues({ name: 'Anuj' });
+    expect(injected.values.name).toEqual('Anuj');
   });
 });
